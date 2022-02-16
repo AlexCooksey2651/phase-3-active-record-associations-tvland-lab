905b54d3ef620b4f3fdec2416afcec44f16ce048
Alex Cooksey <aecooksey2651@gmail.com>
2022-02-16 14:46:44 -0500

    Need to test, may have migrated too early

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,25 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def characters
+    self.characters
+  end
+
+  def shows
+    self.shows
+  end
+
+  def full_name
+    full_name = "#{self.first_name} + #{self.last_name}"
+    full_name
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each do |character|
+        roles << "#{character} - #{character.show}"
+    end
+    roles
+  end
 end
